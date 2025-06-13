142373ea5d3ff65dad5cd3596f9e4d0decb08684
carol-changes <carolvianaxx@gmail.com>
2025-06-12 21:12:58 -0300

    Layout update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {DeployListComponent} from './components/deploy-list/deploy-list.component';
+import {DeployDetailComponent} from './components/deploy-detail/deploy-detail.component';
+import {DeployViewComponent} from './components/deploy-view/deploy-view.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+  { path: '',
+    redirectTo: 'deploys',
+    pathMatch: 'full' },
+
+  {
+    path: 'deploys',
+    component: DeployListComponent,
+  },
+  {
+    path: 'deploys/new',
+    component: DeployDetailComponent,
+  },
+  {
+    path: 'deploys/:id',
+    component: DeployViewComponent,
+  }
+  ,
+  {
+    path: 'deploys/edit/:id',
+    component: DeployDetailComponent,
+  },
+  {
+    path: '**',
+    redirectTo: "deploys",
+  },
+
+
+];
